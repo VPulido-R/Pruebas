1785f756bdcd63e361c066ca80aa6a18d7df2163
raspberry <ingridvanessa5715@hotmail.com>
2025-08-25 13:18:07 -0600

    Pruebas con codigo base

---
+++ generacodigoqr.py
@@ -0,0 +1,5 @@
+#CODIGO PARA CREACION DE CODIGOS QR :)
+import qrcode
+code= qrcode.make('VanessaPulido,RD19')
+code.save("vanessa_pulido.png")
+
--- prueba1.py
+++
@@ -1,5 +0,0 @@
-#CODIGO PARA CREACION DE CODIGOS QR :)
-import qrcode
-code= qrcode.make('VanessaPulido,RD19')
-code.save("vanessa_pulido.png")
-
